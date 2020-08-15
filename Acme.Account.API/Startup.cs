4c2d728c2d44ec4b0b83b9f9a2b8afa32ec4e91c
Arvind Mishra <arvind.mishra1991@outlook.com>
2020-08-16 00:23:10 +0530

    added swagger doc

--- Acme.Account.API/Startup.cs
+++ Acme.Account.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Acme.Account.API
 {
@@ -24,6 +25,12 @@ namespace Acme.Account.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ACME Account API", Version = "v1", Description = "ACME bank account management API" });
+            });
+
             services.AddControllers();
         }
 
@@ -34,6 +41,14 @@ namespace Acme.Account.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ACME Account Management V1");
+                //c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
