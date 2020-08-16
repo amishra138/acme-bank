6c695f796436cb337c6edb66f1eb2c38da3cec8c
Arvind Mishra <arvind.mishra1991@outlook.com>
2020-08-16 13:53:26 +0530

    added repository and db context

---
+++ Acme.Account.Infrastructure/AcmeContext.cs
@@ -0,0 +1,40 @@
+﻿using Acme.Account.Core.Models;
+using Acme.Account.Infrastructure.EntityConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Acme.Account.Infrastructure
+{
+    public class AcmeContext : DbContext
+    {
+        public const string DEFAULT_SCHEMA = "AcmeDb";
+
+        public AcmeContext(DbContextOptions<AcmeContext> options) : base(options)
+        {
+            AcmeContextInitializer.SetInitializer
+        }
+
+        public DbSet<CustomerModel> Customers { get; set; }
+
+        public DbSet<AccountModel> Accounts { get; set; }
+
+        public DbSet<AddressModel> Addresses { get; set; }
+
+        public DbSet<TransactionModel> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+            builder.ApplyConfiguration(new CustomerEntityConfigurations());
+            builder.ApplyConfiguration(new AccountEntityConfigurations());
+            builder.ApplyConfiguration(new AddressEntityConfigurations());
+            builder.ApplyConfiguration(new TransactionEntityConfigurations());
+        }
+    }
+}
---
+++ Acme.Account.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,83 @@
+﻿using Acme.Account.Core.Models;
+using Acme.Account.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Acme.Account.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private bool _disposed = false;
+        private readonly AcmeContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(AcmeContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            _dbSet.Add(entity);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await GetAsync(id);
+            await DeleteAsync(entity);
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            if (_context.Entry(entity).State == EntityState.Detached)
+                _dbSet.Attach(entity);
+
+            _dbSet.Remove(entity);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetAsync(Guid id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
