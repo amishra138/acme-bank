f68fbeae2081c5b743e1910a6faac52534c6d781
Arvind Mishra <arvind.mishra1991@outlook.com>
2020-08-16 13:53:49 +0530

    added connection string

--- Acme.Account.API/Startup.cs
+++ Acme.Account.API/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Acme.Account.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +32,18 @@ namespace Acme.Account.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ACME Account API", Version = "v1", Description = "ACME bank account management API" });
             });
+
+            services.AddEntityFrameworkSqlServer()
+                   .AddDbContext<AcmeContext>(options =>
+                   {
+                       options.UseSqlServer(Configuration["ConnectionString"],
+                           sqlServerOptionsAction: sqlOptions =>
+                           {
+                               sqlOptions.EnableRetryOnFailure(maxRetryCount: 2, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                           });
+                   },
+                       ServiceLifetime.Scoped  //Showing explicitly that the DbContext is shared across the HTTP request scope (graph of objects started in the HTTP request)
+                   );
 
             services.AddControllers();
         }
